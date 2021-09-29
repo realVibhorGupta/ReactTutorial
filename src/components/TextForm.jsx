72494d76ddffb54e503e1cfa01a7fa98f58bcb65
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-09-29 09:29:30 +0000

    Added few features

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -11,7 +11,18 @@ const handleUpperCase = () =>{
   let resultText = text.toUpperCase();
   setText(resultText);
 }
+const handleLowerCase = () =>{
+  console.log("Lower case button Clicked "+ text);
 
+  let resultText = text.toLowerCase();
+  setText(resultText);
+}
+const handleClearText = () =>{
+  console.log("ClearText  button Clicked "+ text);
+
+  let resultText = ""
+  setText(resultText);
+}
 const handleOnChange = (event) =>{
   console.log("OnChange");
 
@@ -27,9 +38,18 @@ const [text , setText] = useState('Enter Text Here');
       <label for = "myBox"  class="form-label">
       {props.title}
       </label>
-      <textarea className ="form-control"  value={text} id ="myBox" rows = "6"onClick={handleOnChange}/>
+      <textarea className ="form-control"   onClick={handleOnChange} value={text} id ="myBox" rows = "6"/>
     </div>
-    <button className="btn btn-primary" onClick={handleUpperCase}>Convert to Uppercase</button>
+    <button className="btn btn-primary" onClick={handleUpperCase} > Convert to Uppercase </button>
+    <button className="btn btn-primary mx-3" onClick={handleLowerCase} > Convert to Lowercase </button>
+     <button className="btn btn-primary mx-3 my-3" onClick={handleClearText} >Clear Text</button>
+      <div className="container my-3">
+              <h1>Your Text Summary</h1>
+              <p> {text.split(" ").length} Words , {text.length} characters</p>
+              <p> {0.005 * text.split(" ").length } minutes read</p>
+              <h2 className = "my-3">Preview</h2>
+              <p>{text}</p>
+       </div>
     </>
   );
 
