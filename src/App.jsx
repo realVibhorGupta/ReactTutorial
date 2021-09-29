f79213809a1dcd3205db78789fe7b9e7e1409b5d
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-09-29 06:45:48 +0000

    Navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar";
+
 
 function App() {
+ 
   return (
-    <main>
-     
-    </main>
+    <>
+    <Navbar/>
+   
+
+    
+    </>
   );
 }
 
